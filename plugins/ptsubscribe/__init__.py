7203d01919c7f4150407906010da9f371b3e357c
Samdy.fang <Samdy.fang@tpv-tech.com>
2025-03-17 09:48:38 +0800

    fix some issue2

--- plugins/ptsubscribe/__init__.py
+++ plugins/ptsubscribe/__init__.py
@@ -35,7 +35,7 @@ class PtSubscribe(_PluginBase):
     # 插件图标
     plugin_icon = "Zerotier_A.png"
     # 插件版本
-    plugin_version = "2.0"
+    plugin_version = "2.1"
     # 插件作者
     plugin_author = "bestfang"
     # 作者主页
